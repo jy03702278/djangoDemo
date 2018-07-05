4d0de10aaa1544b7596762d427a6743c4cd0c9e0
jy03702278 <18601375679@163.com>
2018-07-05 23:01:48 +0800

    填写了form表单

--- sign/views.py
+++ sign/views.py
@@ -6,4 +6,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello World!')
+    # return HttpResponse('Hello World!')
+    #使用Django的render函数地方,默认找到templates目录下的指定的html文件
+    return render(request,"index.html")
