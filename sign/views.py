4d17664a20802d0425bbafb1f80d73b4b1547289
jy03702278 <18601375679@163.com>
2018-07-10 23:46:19 +0800

    使用session记录sessionid到浏览器 使用auth模块认证表单的用户名密码

--- sign/views.py
+++ sign/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.contrib import auth
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 
@@ -16,15 +16,22 @@ def login_action(request):
     if request.method == 'POST':
         username = request.POST.get('username','')
         password = request.POST.get('password', '')
-        if username == 'admin' and password == 'admin123':
+        #这里不用if username。。。password。。。判断。可以使用auth的authenticate函数认证用户名和密码，并返回一个user对象
+        #如果user对象不为None，说明用户认证通过，然后调用auth的login函数进行登录
+        user = auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user) #login函数有两个参数，一个HTTPRequest对象，一个user对象
+        # if username == 'admin' and password == 'admin123':
             #重定向路径，并设置cookie，（cookie名字，cookie内容，cookie在浏览器上保存的时间秒数）
             response = HttpResponseRedirect('/event_manage/')
-            response.set_cookie('user',username,3600)
+            # response.set_cookie('user',username,3600) #添加浏览器cookie
+            request.session['user'] = username #将session信息记录到浏览器
             return response
         else:
             #render函数第三个参数，是字典，显示key对应的value的错误信息.html页面要用{{ error}}，才可以显示对应的内容
             return render(request,'index.html',{'error':'username or password error !'})
 
 def event_manage(request):
-    username = request.COOKIES.get('user','')
+    # username = request.COOKIES.get('user','') #读取浏览器cookie
+    username = request.session.get('user','') #读取浏览器session
     return render(request,"event_manage.html",{'user':username})
